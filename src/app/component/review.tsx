05af7357b8f9e2186e08272e44703f09f2fe321c
Zaibunis <zainabmustaqeem123@hotmail.com>
2024-12-21 10:29:00 +0500

    new

--- src/app/component/review.tsx
+++ src/app/component/review.tsx
@@ -13,7 +13,7 @@ const Review = () => {
         OUR HAPPY CUSTOMERS
       </h1>
     </div>
-    <div className="flex flex-wrap -m-4">
+    <div className="flex flex-wrap -m-4 border-b-2">
       <div className="p-4 md:w-1/3">
         <div className="flex rounded-lg h-full bg-white border-b-2 p-8 flex-col">
           <div className="flex items-center mb-3">
